98cd0ceffc5e222a061f67cad35c402906e43096
malabya <malabya35@gmail.com>
2018-05-09 23:39:03 +0530

    Blog section

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,44 @@
 $(document).foundation();
+
+(function ($) {
+
+    /**
+     * Copyright 2012, Digital Fusion
+     * Licensed under the MIT license.
+     * http://teamdf.com/jquery-plugins/license/
+     *
+     * @author Sam Sehnert
+     * @desc A small plugin that checks whether elements are within
+     *     the user visible viewport of a web browser.
+     *     only accounts for vertical position, not horizontal.
+     */
+
+    $.fn.visible = function (partial) {
+
+        let $t = $(this),
+            $w = $(window),
+            viewTop = $w.scrollTop(),
+            viewBottom = viewTop + $w.height(),
+            _top = $t.offset().top + 100,
+            _bottom = _top + $t.height(),
+            compareTop = partial === true ? _bottom : _top,
+            compareBottom = partial === true ? _top : _bottom;
+        console.log(viewTop);
+        return ((compareBottom <= viewBottom) && (compareTop >= viewTop));
+
+    };
+
+
+    $(window).scroll(function (event) {
+
+        $('.animation-block').each(function (i, el) {
+            let el = $(el);
+            if (el.visible(true)) {
+                el.removeClass('not-loaded');
+                el.addClass('loaded');
+            }
+        });
+
+    });
+})(jQuery);
+
